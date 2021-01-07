cbefad6170f6378de2b2e116430324d93a230a90
HaoningLan <546004464@qq.com>
2021-01-07 13:12:48 +0800

    init

---
+++ main.py
@@ -0,0 +1,42 @@
+import base64
+
+import cv2
+from flask import jsonify
+from flask import Flask
+from flask import request
+from pyimagesearch.scan import image_scan
+import numpy as np
+
+
+def cv2_base64(image):
+    image1 = cv2.imencode('.png', image)[1]
+    base64_str = str(base64.b64encode(image1))[2:-1]
+    return base64_str
+
+
+def base64_cv2(base64_str):
+    imgString = base64.b64decode(base64_str)
+    nparr = np.frombuffer(imgString, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return image
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    data = request.get_json()
+    img = data['data'].split(',')[1]
+
+    cv2img = base64_cv2(img)
+    wraped = image_scan(cv2img)
+    cv2.imshow("Image1", wraped)  # 显示图片
+    cv2.waitKey()
+    base64img = cv2_base64(wraped)
+
+    return jsonify({"image": "data:image/png;base64," + base64img})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
